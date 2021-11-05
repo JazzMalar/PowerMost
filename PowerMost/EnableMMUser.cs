4179b6f470f15d9ba1f314d91c0e61c02351640c
Markus Schenk <ms@theschenks.ch>
2021-11-05 15:37:59 +0100

    Added Powershell Cmdlets to Get and Enable a user

---
+++ PowerMost/EnableMMUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace PowerMost
+{
+    [Cmdlet(VerbsLifecycle.Enable, "MMUser")]
+    public class EnableMMUser : BaseClass
+    {
+        [Parameter(Mandatory = true, Position = 1)]
+        public string username { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            api.EnableUser(username); 
+        }
+
+    }
+}
---
+++ PowerMost/GetMMUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace PowerMost
+{
+    [Cmdlet(VerbsCommon.Get, "MMUser")]
+    public class GetMMUser : BaseClass
+    {
+        [Parameter(Mandatory = true, Position = 1)]
+        public string username { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(api.GetUserByUserName(username)); 
+        }
+
+    }
+}
